cde90457751ae4b79642e6925142989c47f3f9c6
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-10-24 02:02:17 +0530

    map key added

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,10 @@ export default async function Home() {
       <Header />
       <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
         {restaurants.map((restaurant) => (
-          <RestaurantCard restaurant={restaurant} />
+          <RestaurantCard
+            key={restaurant.id}
+            restaurant={restaurant}
+          />
         ))}
       </div>
     </main>
